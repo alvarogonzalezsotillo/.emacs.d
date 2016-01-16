c725f1ec81f8f85d9e4e33e76280afc81879f3db
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2016-01-16 18:25:58 +0100

    Porque sí

---
+++ elpa/flx-20151030.1112/flx-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flx" "20151030.1112" "fuzzy matching with good sorting" (quote ((cl-lib "0.3"))))
---
+++ elpa/scala-outline-popup-20150702.937/scala-outline-popup-pkg.el
@@ -0,0 +1,1 @@
+(define-package "scala-outline-popup" "20150702.937" "scala file summary popup" (quote ((dash "2.9.0") (popup "0.5.3") (scala-mode2 "0.22") (flx-ido "0.5"))))
