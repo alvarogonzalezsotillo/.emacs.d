7a7b304efbb74787919c913b1a4c002b3a37749b
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2016-03-02 09:55:34 +0100

    Habilitado grabar el desktop, quitados números de línea

--- init.el
+++ init.el
@@ -66,8 +66,8 @@
 (global-set-key (kbd "C-x F") 'find-file-in-project)
 
 ;; NUMEROS DE LINEA
-(global-linum-mode t)
-
+;;(global-linum-mode t)
+;;(global-linum-mode nil)
 
 ;; ECB
 (require 'ecb-autoloads)
@@ -101,9 +101,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
  '(chess-default-display (quote (chess-images chess-ics1 chess-plain)))
  '(custom-enabled-themes (quote (tsdh-dark)))
+ '(desktop-save-mode t)
  '(fill-column 120)
  '(line-number-mode nil)
  '(linum-mode 1 t)
