991d19918bb7bf5c096a0c7e5c705946ccab3d93
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2016-06-06 20:21:09 +0200

    algo

---
+++ elpa/transpose-frame-20151126.626/transpose-frame-autoloads.el
@@ -0,0 +1,58 @@
+;;; transpose-frame-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "transpose-frame" "transpose-frame.el" (22351
+;;;;;;  64144 270775 729000))
+;;; Generated autoloads from transpose-frame.el
+
+(autoload 'transpose-frame "transpose-frame" "\
+Transpose windows arrangement at FRAME.
+Omitting FRAME means currently selected frame.
+
+\(fn &optional FRAME)" t nil)
+
+(autoload 'flip-frame "transpose-frame" "\
+Flip windows arrangement vertically at FRAME.
+Omitting FRAME means currently selected frame.
+
+\(fn &optional FRAME)" t nil)
+
+(autoload 'flop-frame "transpose-frame" "\
+Flop windows arrangement horizontally at FRAME.
+Omitting FRAME means currently selected frame.
+
+\(fn &optional FRAME)" t nil)
+
+(autoload 'rotate-frame "transpose-frame" "\
+Rotate windows arrangement 180 degrees at FRAME.
+Omitting FRAME means currently selected frame.
+
+\(fn &optional FRAME)" t nil)
+
+(autoload 'rotate-frame-clockwise "transpose-frame" "\
+Rotate windows arrangement 90 degrees clockwise at FRAME.
+Omitting FRAME means currently selected frame.
+
+\(fn &optional FRAME)" t nil)
+
+(autoload 'rotate-frame-anticlockwise "transpose-frame" "\
+Rotate windows arrangement 90 degrees anti-clockwise at FRAME.
+Omitting FRAME means currently selected frame.
+
+\(fn &optional FRAME)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("transpose-frame-pkg.el") (22351 64144
+;;;;;;  313332 588000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; transpose-frame-autoloads.el ends here
---
+++ elpa/transpose-frame-20151126.626/transpose-frame-pkg.el
@@ -0,0 +1,5 @@
+(define-package "transpose-frame" "20151126.626" "Transpose windows arrangement in a frame" 'nil :keywords
+  '("window"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
