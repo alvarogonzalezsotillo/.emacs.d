91170ba427ef298a398cf2aba66c7d591ac4324c
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2016-12-02 13:00:28 +0100

    c-z es deshacer, selección en org-mode

--- init.el
+++ init.el
@@ -278,6 +278,23 @@
 (define-key yas-minor-mode-map (kbd "\C-c TAB") 'yas-expand)
 
 
+;; MIS TECLAS
+(defvar mis-teclas-minor-mode-map
+  (let ((map (make-sparse-keymap)))
+    ;(define-key map (kbd "C-i") 'some-function)
+    (define-key map (kbd "C-e") 'er/expand-region)
+    (define-key map (kbd "C-S-e") 'er/contract-region)
+    (define-key map (kbd "C-z") 'undo )
+    map)
+  "mis-teclas-minor-mode keymap.")
+
+(define-minor-mode mis-teclas-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  :init-value t
+  :lighter " mist-teclas")
+
+(mis-teclas-minor-mode 1)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -297,7 +314,7 @@
  '(custom-enabled-themes (quote (alvaro-emacs-theme)))
  '(custom-safe-themes
    (quote
-    ("12cd2ff2db62c2fe561fa6148e5438c6e0eebb9daa7b46c69931ffacfee1521d" "3a0083b2db70cff2c828d59c37973384a9d2f07b3911e8292c19b3c701552804" "c8bb12b86341bfdc154664bf93fc0753ba2ea91c85b9f678e664288c1dd74d05" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "36d92f830c21797ce34896a4cf074ce25dbe0dabe77603876d1b42316530c99d" "b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+    ("fb2f0a401501100076f32b6e4ca8cdc9c7d943a7099f2085323ffa1a460819f6" "12cd2ff2db62c2fe561fa6148e5438c6e0eebb9daa7b46c69931ffacfee1521d" "3a0083b2db70cff2c828d59c37973384a9d2f07b3911e8292c19b3c701552804" "c8bb12b86341bfdc154664bf93fc0753ba2ea91c85b9f678e664288c1dd74d05" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "36d92f830c21797ce34896a4cf074ce25dbe0dabe77603876d1b42316530c99d" "b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(delete-selection-mode t)
  '(desktop-save t)
  '(desktop-save-mode t)
@@ -312,6 +329,11 @@
                  ("begin" "$1" "$" "$$" "\\(" "\\["))))
  '(org-latex-default-table-environment "longtable")
  '(org-latex-images-centered nil)
+ '(org-src-block-faces (quote (("shell" default) ("js" default))))
+ '(org-support-shift-select t)
+ '(package-selected-packages
+   (quote
+    (org markdown-mode magit popup-complete scad-preview scad-mode org-attach-screenshot bm yafolding web-mode transpose-frame tablist switch-window swiper sr-speedbar smartparens scala-outline-popup request-deferred rectangle-utils rainbow-delimiters php-mode page-break-lines ox-reveal org-present org-ac neotree multiple-cursors image+ htmlize helm-projectile guide-key-tip github-browse-file git-timemachine git-link flycheck find-file-in-project expand-region epresent ensime discover diffview crappy-jsp-mode company-auctex chess calfw browse-at-remote auto-highlight-symbol auto-complete-auctex alert adaptive-wrap)))
  '(preview-TeX-style-dir "/home/alvaro/.emacs.d/elpa/auctex-11.89.6/latex")
  '(preview-default-preamble
    (quote
@@ -396,10 +418,10 @@
  '(font-lock-comment-face ((t (:foreground "peru"))))
  '(neo-dir-link-face ((t (:height 0.8))))
  '(neo-file-link-face ((t (:foreground "White" :height 0.8))))
- '(org-block ((t (:inherit shadow :inverse-video t :family "courier"))))
+ '(org-block ((t (:inherit shadow :background "gray10"))))
  '(org-level-1 ((t (:inherit outline-1 :box (:line-width 2 :color "grey75" :style released-button) :height 2.0))))
  '(org-level-2 ((t (:inherit outline-2 :box nil :height 1.5))))
- '(org-meta-line ((t (:inherit font-lock-comment-face :height 0.4))))
+ '(org-meta-line ((t (:inherit font-lock-comment-face :height 1.0))))
  '(preview-reference-face ((t (:background "magenta")))))
 (put 'LaTeX-narrow-to-environment 'disabled nil)
 (put 'TeX-narrow-to-group 'disabled nil)
