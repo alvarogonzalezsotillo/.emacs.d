f4c7bd7bffb2805ef88aa2fd6273e09269da6829
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2017-11-28 17:10:38 +0100

    Me cargo el tema, al init.el

--- init.el
+++ init.el
@@ -116,9 +116,9 @@
 
 ;; MAGIT STATUS
 (global-set-key (kbd "<f9>") 'magit-status)
-(setenv "GIT_TRACE" "2")
-(setenv "GIT_CURL_VERBOSE" "2")
-(setenv "GIT_TRACE_PACKET" "2")
+;(setenv "GIT_TRACE" "1")
+;(setenv "GIT_CURL_VERBOSE" "1")
+;(setenv "GIT_TRACE_PACKET" "1")
 
 ;; DIRECTORIOS DE BACKUP
 (setq backup-directory-alist `(("." . "~/.saves")))
@@ -383,6 +383,21 @@ contextual information."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+  '(TeX-source-correlate-start-server t)
+ '(ac-ignore-case nil)
+ '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+ '(desktop-save t)
+ '(fill-column 120)
+ '(linum-mode 1)
+ '(neo-hidden-regexp-list (quote ("^\\." "\\.pyc$" "~$" "^#.*#$" "\\.elc$" ".git")))
+ '(neo-smart-open t)
+ '(neo-theme (quote nerd))
+ '(org-babel-load-languages (quote ((emacs-lisp . t) (shell . t))))
+ '(org-html-mathjax-options (quote ((path "//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML") (scale "100") (align "center") (font "TeX") (linebreaks "false") (autonumber "AMS") (indent "0em") (multlinewidth "85%") (tagindent ".8em") (tagside "right"))))
+ '(org-latex-image-default-width "1cm")
+ '(org-latex-listings t)
+ '(package-selected-packages (quote (markdown-mode magit popup-complete scad-preview scad-mode org-attach-screenshot bm yafolding web-mode transpose-frame tablist switch-window swiper sr-speedbar smartparens scala-outline-popup request-deferred rectangle-utils rainbow-delimiters php-mode page-break-lines ox-reveal org-present org-ac neotree multiple-cursors image+ htmlize helm-projectile guide-key-tip github-browse-file git-timemachine git-link flycheck find-file-in-project expand-region epresent ensime discover diffview crappy-jsp-mode company-auctex chess calfw browse-at-remote auto-highlight-symbol auto-complete-auctex alert adaptive-wrap)))
+ '(send-mail-function (quote sendmail-send-it))
  '(LaTeX-command-style
    (quote
     (("" "%(PDF)%(latex) %(file-line-error) -shell-escape %(extraopts) %S%(PDFout)"))))
@@ -404,7 +419,6 @@ contextual information."
                       (company-dabbrev-code company-gtags company-etags company-keywords)
                       company-oddmuse company-dabbrev)))
  '(company-show-numbers t)
- '(custom-enabled-themes (quote (alvaro-emacs-theme)))
  '(custom-safe-themes
    (quote
     ("fb2f0a401501100076f32b6e4ca8cdc9c7d943a7099f2085323ffa1a460819f6" "12cd2ff2db62c2fe561fa6148e5438c6e0eebb9daa7b46c69931ffacfee1521d" "3a0083b2db70cff2c828d59c37973384a9d2f07b3911e8292c19b3c701552804" "c8bb12b86341bfdc154664bf93fc0753ba2ea91c85b9f678e664288c1dd74d05" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "36d92f830c21797ce34896a4cf074ce25dbe0dabe77603876d1b42316530c99d" "b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
@@ -440,7 +454,6 @@ contextual information."
    (quote
     (latex-preview-pane markdown-preview-mode helm-ag dumb-jump lorem-ipsum calfw-ical web-beautify gitignore-mode use-package company-restclient ob-restclient restclient-helm restclient transmission hl-line+ paradox gift-mode org-webpage plsql org-page company-web company-shell company-quickhelp company-emoji company-c-headers company company-auctex helm-company highlight-indent-guides which-key dired-narrow org markdown-mode magit popup-complete scad-preview scad-mode org-attach-screenshot bm yafolding web-mode transpose-frame tablist switch-window swiper smartparens scala-outline-popup request-deferred rectangle-utils php-mode page-break-lines ox-reveal org-present neotree multiple-cursors image+ htmlize helm-projectile git-timemachine flycheck expand-region ensime diffview crappy-jsp-mode chess calfw auto-highlight-symbol alert adaptive-wrap)))
  '(paradox-github-token t)
- '(preview-TeX-style-dir "/home/alvaro/.emacs.d/elpa/auctex-11.89.6/latex")
  '(preview-default-preamble
    (quote
     ("\\RequirePackage["
@@ -529,9 +542,18 @@ contextual information."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "gray10" :foreground "white smoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "DAMA" :family "Ubuntu Mono"))))
+  '(font-lock-comment-face ((t (:foreground "peru"))))
+ '(neo-dir-link-face ((t (:height 0.8))))
+ '(neo-file-link-face ((t (:foreground "White" :height 0.8))))
+ '(org-block ((t (:inherit shadow :inverse-video t :family "courier"))))
+ '(org-level-1 ((t (:inherit outline-1 :box (:line-width 2 :color "grey75" :style released-button) :height 2.0))))
+ '(org-level-2 ((t (:inherit outline-2 :box nil :height 1.5))))
+ '(org-meta-line ((t (:inherit font-lock-comment-face :height 0.4))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white smoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
  '(font-lock-comment-face ((t (:foreground "peru"))))
  '(hl-line ((t (:background "gray30"))))
+ '(magit-branch-local ((t (:background "dark red" :foreground "LightSkyBlue1"))))
+ '(magit-branch-remote ((t (:background "dark magenta" :foreground "DarkSeaGreen2"))))
  '(neo-dir-link-face ((t (:height 0.8))))
  '(neo-file-link-face ((t (:foreground "White" :height 0.8))))
  '(org-block ((t (:inherit shadow :background "gray10"))))
