c4b7022e5e229da3663f77a70b591d6918c712f8
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2018-02-01 20:18:30 +0100

    error en CF5

--- init.el
+++ init.el
@@ -55,7 +55,13 @@
   (org-latex-export-to-pdf)
   (let* (
         (filename (buffer-file-name))
-        (tex-filename (concat (file-name-sans-extension filename) ".tex")) )
+        (tex-filename (concat (file-name-sans-extension filename) ".tex"))
+        (html-filename (concat (file-name-sans-extension filename) ".html"))
+        (html-reveal-filename (concat (file-name-sans-extension filename) ".reveal.html")) )
+
+    (message "renombrando fichero: %s -> %s" html-filename html-reveal-filename)
+    (rename-file html-filename html-reveal-filename t)
+    
     (message "Borrando fichero: %s" tex-filename)
     (delete-file tex-filename) )
  )
