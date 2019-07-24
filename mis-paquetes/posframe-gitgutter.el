38d261e1ec75c0ef97ce4e03f98a122e4538250e
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2019-07-24 10:18:35 +0200

    posframe-gitgutter

--- mis-paquetes/posframe-gitgutter.el
+++ mis-paquetes/posframe-gitgutter.el
@@ -2,57 +2,65 @@
 (require 'posframe)
 (require 'git-gutter)
 
+(defvar posframe-gitgutter-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key global-map (kbd "<left-margin> <mouse-1>") 'posframe-gitgutter--click)
+    (define-key global-map (kbd "<right-margin> <mouse-1>") 'posframe-gitgutter--click)
+    map)
+  "Keymap for posframe-gitgutter-mode.")
+
+(defvar posframe-gitgutter-buffer-name "*posframe-gitgutter-hunk*" "Name of the posframe used by posframe-gitgutter.")
+
+(defvar posframe-gitgutter-textscale 2 "Decrease of the posframe-gitgutter font.")
+
 
 (defun posframe-gitgutter--post-command-hook ()
-  (message "this-command:%s" this-command)
+  "Post command hook to delete the posframe."
   (when (not (string= this-command "posframe-gitgutter--click"))
-    (posframe-gitgutter--delete-posframe)
-    (posframe-delete "*posframe-gitgutter-hunk*")
+    (posframe-delete posframe-gitgutter-buffer-name)
     (remove-hook 'post-command-hook 'posframe-gitgutter--post-command-hook t)))
 
 
 (defun posframe-gitgutter--click (event)
   "Called when user clicks on margins.  EVENT holds click information."
   (interactive "event")
-  
-  (let*
-      ((start (event-start event))
-       (position (posn-point start))
-       (window (posn-window start)))
-    
-    (posn-set-point start)
 
+  (add-hook 'post-command-hook 'posframe-gitgutter--post-command-hook t t)
+  (when (posframe-workable-p)
     (let*
-        (
-         (info (git-gutter:search-here-diffinfo git-gutter:diffinfos))
-         (raw-content (git-gutter-hunk-content info))
-         (content (mapconcat 'identity (cdr (split-string raw-content "\n")) "\n"))
-         )
-      (posframe-show  "*posframe-gitgutter-hunk*"
-                      :string content
-                      :position position
-                      :internal-border-width 10
-                      :internal-border-color "#00FFFF"
-                      :foreground-color "#ff00ff"
-                      :background-color "#00ff00")
+        ((start (event-start event))
+         (position (posn-point start))
+         (window (posn-window start)))
+      
+      (posn-set-point start)
 
-      (add-hook 'post-command-hook 'posframe-gitgutter--post-command-hook t)
+      (let*
+          (
+           (info (git-gutter:search-here-diffinfo git-gutter:diffinfos))
+           (raw-content (git-gutter-hunk-content info))
+           (content (mapconcat 'identity (cdr (split-string raw-content "\n")) "\n"))
+           )
+        (posframe-show  posframe-gitgutter-buffer-name
+                        :string content
+                        :position position
+                        :internal-border-width 2
+                        :internal-border-color "#00FFFF")
+
+
+        (with-current-buffer posframe-gitgutter-buffer-name
+          (diff-mode)
+          (text-scale-decrease posframe-gitgutter-textscale)
+          
+          )
+
+        
+        )
+      
+      (add-hook 'post-command-hook 'posframe-gitgutter--post-command-hook t t)
+
+
       )
-
-
-    )
-  )
-
-
-
-
-
-(defvar posframe-gitgutter-mode-map
-  (let ((map (make-sparse-keymap)))
-    (define-key global-map (kbd "<left-margin> <mouse-1>") 'posframe-gitgutter--click')
-    (define-key global-map (kbd "<right-margin> <mouse-1>") 'posframe-gitgutter--click')
-    map)
-  "Keymap for posframe-gitgutter-mode.")
+    ))
 
 
 ;;;###autoload
@@ -60,7 +68,6 @@
   "Enables the margin to show a posframe with git diffs when clicked."
   :group posframe-gitgutter-group
 
-  
   (setq posframe-mouse-banish nil))
 
 
